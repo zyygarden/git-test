223231455a09966a6d3b7e0f10abe006ce545ac0
zyygarden <95735570+zyygarden@users.noreply.github.com>
2022-06-24 15:20:16 +0800

    Update GitTest.java

--- src/main/java/com/yuyang/git/GitTest.java
+++ src/main/java/com/yuyang/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master commit5");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
+        System.out.println("我改代码了哈哈哈哈哈哈哈");
     }
 }
